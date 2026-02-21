95026519c55460f88f196f682f15d10140918b94
Mirlan Rysbekov <rys5ek0v.111@gmail.com>
2026-02-21 18:21:30 +0600

    Set up project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ProfileIcon from '/icons/profile.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <img src={ProfileIcon} alt="profile" />
+      </header>
+    </>
+  )
+}
+
+export default App
